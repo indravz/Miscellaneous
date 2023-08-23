380b15bc7ec2ee40417f0d549c2f30cf431f76e3
indravz <indrareddy.eee@gmail.com>
2023-08-23 15:52:26 -0500

    Update frontend.tsx

--- frontend.tsx
+++ frontend.tsx
@@ -7,3 +7,18 @@ if (typeof window !== 'undefined') {
   const url = 'https://www.google.com'; // Replace with your desired URL
   window.location.replace(url);
 }
+
+
+componentDidMount() {
+    const { pathname } = window.location;
+    let redirectUrl = 'https://www.google.com/default';
+
+    if (pathname === '/home') {
+      redirectUrl = 'https://www.google.com/home';
+    } else if (pathname === '/app') {
+      redirectUrl = 'https://www.google.com/app';
+    }
+
+    // Redirect to the calculated URL
+    window.location.replace(redirectUrl);
+  }
