67beb4532a82d55a687865bfb6e085c270b8345d
indravz <indrareddy.eee@gmail.com>
2023-08-22 23:34:53 -0500

    Update frontend.tsx

--- frontend.tsx
+++ frontend.tsx
@@ -2,3 +2,8 @@ async componentDidMount() {
     // Redirect to the desired URL (e.g., google.com)
     window.location.replace('https://www.google.com');
   }
+
+if (typeof window !== 'undefined') {
+  const url = 'https://www.google.com'; // Replace with your desired URL
+  window.location.replace(url);
+}
