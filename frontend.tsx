563bdd1228f91e7ba2f5cd4576bd8ca9b85be4a8
indravz <indrareddy.eee@gmail.com>
2023-08-23 16:36:59 -0500

    Update frontend.tsx

--- frontend.tsx
+++ frontend.tsx
@@ -22,3 +22,46 @@ componentDidMount() {
     // Redirect to the calculated URL
     window.location.replace(redirectUrl);
   }
+
+#####################################################################
+
+
+import * as React from 'react';
+import NextApp from 'next/app';
+import { Provider } from 'urql';
+
+import ErrorPage from '@components/error-page';
+import client from '@utils/graphql-client';
+
+import 'normalize.css';
+import '@/styles/global.scss';
+import '@/styles/fonts.scss';
+
+interface AppProps {
+  err?: Error;
+}
+
+class App extends NextApp<AppProps> {
+  // Your existing code for componentDidMount, getInitialProps, and render
+
+  // ...
+}
+
+export default App;
+
+// Immediately perform the redirect
+if (typeof window !== 'undefined') {
+  const urlMap = {
+    '/home': 'https://www.google.com/home',
+    '/app': 'https://www.google.com/app',
+    '/contact': 'https://www.example.com/contact',
+    // Add more paths and their corresponding redirect URLs as needed
+    default: 'https://www.google.com/default',
+  };
+
+  const currentPath = window.location.pathname;
+  const redirectUrl = urlMap[currentPath] || urlMap.default;
+
+  window.location.replace(redirectUrl);
+}
+
